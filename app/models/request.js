f492b014cc7d6ff3e8e644fae70030eb50027849
Sophie Orta <sophie@travis-ci.org>
2020-03-30 18:04:38 -0400

    Try 3.13 first, fix a couple of things

--- app/models/request.js
+++ app/models/request.js
@@ -58,8 +58,10 @@ export default Model.extend({
   isDraft: equal('pullRequestMergeable', PULL_REQUEST_MERGEABLE.DRAFT),
 
   messages: computed('repo.id', 'build.request.id', 'fetchMessages.last.value', function () {
+    const repoId = this.get('repo.id');
+    const requestId = this.get('build.request.id');
     const messages = this.fetchMessages.get('lastSuccessful.value');
-    if (!messages) {
+    if (repoId && requestId && !messages) {
       this.fetchMessages.perform();
     }
     return messages || [];
